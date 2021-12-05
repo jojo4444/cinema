945727be343263266938f0f04d436b8d1a86d6c5
giordano.ch <jojowork4444@gmail.com>
2021-12-05 17:15:53 +0200

    add people: Child-Midd-Senior

---
+++ people/people.cpp
@@ -0,0 +1,64 @@
+//
+// Created by jojo on 03.12.2021.
+//
+
+#include "people.h"
+
+people::Person::Person(Age a, std::string city, double cf_active) : a_(a), city_(city), cf_active_(cf_active) {
+    ///
+}
+
+double people::Person::getCf(Age a) {
+    switch (a) {
+        case Age::Middle:
+            return 1. / 30;
+        case Age::Senior:
+            return 1. / 90;
+        case Age::Child:
+            return 1. / 10;
+        case Age::naa:
+            return 0;
+    }
+    return 0;
+}
+
+std::string people::Person::getCity() const {
+    return city_;
+}
+
+double people::Person::goCinema() {
+    return cnet::CinemaNetwork::singleton->buySeat(city_);
+}
+
+people::Child::Child(std::string city) : Person(Age::Child, city, Person::getCf(Age::Child)) {
+    ///
+}
+
+double people::Child::rating(double quality) const {
+    if (std::abs(quality) < 1e-5) {
+        return 0; /// no cinema :(
+    }
+    return 1; /// all time -> super!
+}
+
+people::Midd::Midd(std::string city) : Person(Age::Child, city, Person::getCf(Age::Child)) {
+    ///
+}
+
+double people::Midd::rating(double quality) const {
+    if (std::abs(quality) < 1e-5) {
+        return 0; /// no cinema :(
+    }
+    return thrnd::Thrand::singleton->uniformReal(1 - quality / 10, 1); /// optimism
+}
+
+people::Senior::Senior(std::string city) : Person(Age::Child, city, Person::getCf(Age::Child)) {
+    ///
+}
+
+double people::Senior::rating(double quality) const {
+    if (std::abs(quality) < 1e-5) {
+        return 0; /// no cinema :(
+    }
+    return thrnd::Thrand::singleton->uniformReal(0, quality); /// pessimism
+}
